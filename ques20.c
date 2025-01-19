70008b8f077765983b4af5f0023d5f610c6c0a09
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:23:36 +0530

    Create ques20.c

---
+++ ques20.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Enter a value of the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter a value of the second number: ");
+    scanf("%d", &num2);
+
+    printf("Enter a value of the third number: ");
+    scanf("%d", &num3);
+
+    if (num1 < num2) {
+        if (num1 < num3) {
+            printf("The minimum value is: %d\n", num1);
+        } else {
+            printf("The minimum value is: %d\n", num3);
+        }
+    } else {
+        if (num2 < num3) {
+            printf("The minimum value is: %d\n", num2);
+        } else {
+            printf("The minimum value is: %d\n", num3);
+        }
+    }
+
+    return 0;
+}
