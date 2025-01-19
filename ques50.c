a5e6cfc79b2eb3b6594167dea5bb798203aa1aba
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:37:40 +0530

    Create ques50.c

---
+++ ques50.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;  
+    int i, j;
+
+    for (i = n; i >= 1; i--) {
+        for (j = i; j <= n; j++) {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
