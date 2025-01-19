bbb16ec32e8d2de95ba3343ff07c71e7ce12c4dc
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:16:13 +0530

    Create ques1.c

---
+++ ques1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char name[50]="Heer Jadav";
+    int age=20;
+    char school[100]="abc";
+
+    printf("Name: %s\n",name);
+    printf("Age: %d\n",age);
+    printf("School: %s\n",school);
+
+    return 0;
+}
