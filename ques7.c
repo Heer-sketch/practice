442d6bf27e58c86ca59f89beecdc9ee1c8b00fa3
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:18:55 +0530

    Create ques7.c

---
+++ ques7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int x, y;
+    int result;
+
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+    
+    printf("Enter value of y: ");
+    scanf("%d", &y);
+
+    result = (x + y) * (x + y);
+
+    printf("The result of (%d + %d)^2 is: %d\n", x, y, result);
+
+    return 0;
+}
