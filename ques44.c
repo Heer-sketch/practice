79141e0e05a3387a43d3a1e491b04ff73b6ffc88
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:33:26 +0530

    Create ques44.c

---
+++ ques44.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; 
+    int i, j;
+
+    for (i = 0; i < n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf("%d ", j);
+        }
+
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("  ");
+        }
+
+        for (j = n - i; j >= 1; j--) {
+            if (i != 0 || j != n - i) {  
+                printf("%d ", j);
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
