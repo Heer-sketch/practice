a53999746789ba885b7e1b085333db4acc65749f
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:19:47 +0530

    Create ques9.c

---
+++ ques9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    int result;
+
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+    
+    printf("Enter value of y: ");
+    scanf("%d", &y);
+
+    result = (x + y) * (x + y) * (x + y);
+
+    printf("The result of (%d + %d)^3 is: %d\n", x, y, result);
+
+    return 0;
+}
