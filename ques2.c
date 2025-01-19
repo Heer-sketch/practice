e7381e97f6f1f562a6eb5634c667d4a54a476791
Heer-sketch <heer.k.jadav@gmail.com>
2025-01-19 23:16:40 +0530

    Create ques2.c

---
+++ ques2.c
@@ -0,0 +1,22 @@
+#include <Stdio.h>
+int main(){
+    for (int i=0;i<10;i++)
+    {
+        printf("-");
+
+    }
+    printf("\n");
+
+    printf("|            |\n");
+    printf("R            |\n");
+    printf("N            |\n");
+    printf("W            |\n");
+    printf("|            |\n");
+
+for (int i=0;i<10;i++){
+    printf("-");
+}
+printf("\n");
+
+return 0;
+}   
